00c9d5557ba4654894f89cc189563bcda28523c6
Raffael Tancman <rtancman@gmail.com>
2018-08-14 17:10:32 -0300

    Add create User

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  resources :contacts
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'static_pages#index'
   get 'sobre', to: 'static_pages#about'
   get 'contato', to: 'static_pages#contact'
+  resources :contacts
+  resources :users, only: [:new, :create]
 end
